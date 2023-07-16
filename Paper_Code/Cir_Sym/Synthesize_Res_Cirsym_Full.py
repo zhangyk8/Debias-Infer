4125807f0f144c0c6f42ec3ba99d5b36ffba268d
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2023-07-16 11:30:34 -0700

    Add files via upload

--- Paper_Code/Cir_Sym/Synthesize_Res_Cirsym_Full.py
+++ Paper_Code/Cir_Sym/Synthesize_Res_Cirsym_Full.py
@@ -4,7 +4,7 @@
 @author: Yikun Zhang
 Last Editing: May 11, 2023
 
-Description: Synthesize the slurm array results.
+Description: Synthesize the slurm array results (circulant symmetric covariance).
 """
 
 import numpy as np
@@ -109,8 +109,55 @@ for i in range(5):
                 deb_prog_1se.to_csv('./Results/DebiasProg_Cirsym_cov_homoerr_d'+str(d)+'_n'+str(n)+\
                                     '_x'+str(i)+'_beta'+str(k)+'_'+str(noise)+'_'+str(rule)+'.csv', 
                                     index=False)
-        
 
+     
+# Proposed debiased program (nonparametric propensity score estimation)
+for i in [0,1,2,4]:
+    for k in [0,2]:
+        ## Propensity score estimation (Naive Bayes, Random Forests, SVM, MLP neural network)
+        for non_met in ['NB', 'NBcal', 'RF', 'RFcal', 'SVM', 'SVMcal', 'NN', 'NNcal']:
+            # Different rules for choosing the tuning parameter
+            para_rule = ['1se', 'mincv', 'minfeas']
+            for rule in para_rule:
+                m_deb1_1se = []
+                asym_se1_1se = []  ## Already scaled by sqrt(n)
+                sigma_hat1_1se = []
+                m_deb2_1se = []
+                asym_se2_1se = []  ## Already scaled by sqrt(n)
+                sigma_hat2_1se = []
+                B = 1000
+                for b in range(1, B+1):
+                    try:
+                        with open('./debias_res/DebiasProg_CirSym_cov_homoerr_d'+str(d)+'n'+str(n)+'_MCAR'\
+                                  +str(b)+'_x'+str(i)+'_beta'+str(k)+'_prop_'+str(non_met)+'_'+str(rule)+'.dat', "rb") as file:
+                            m_deb, asym_se, sigma_hat = pickle.load(file)
+                        m_deb1_1se.append(m_deb)
+                        asym_se1_1se.append(asym_se)
+                        sigma_hat1_1se.append(sigma_hat)
+                    except:
+                        print('Nonpar')
+                        print(b)
+                        continue
+                
+                for b in range(1, B+1):
+                    try:
+                        with open('./debias_res/DebiasProg_CirSym_cov_homoerr_d'+str(d)+'n'+str(n)+'_MAR'\
+                                  +str(b)+'_x'+str(i)+'_beta'+str(k)+'_prop_'+str(non_met)+'_'+str(rule)+'.dat', "rb") as file:
+                            m_deb, asym_se, sigma_hat = pickle.load(file)
+                        m_deb2_1se.append(m_deb)
+                        asym_se2_1se.append(asym_se)
+                        sigma_hat2_1se.append(sigma_hat)
+                    except:
+                        print('Nonpar')
+                        print(b)
+                        continue
+                        
+                deb_prog_1se = pd.DataFrame({'m_deb1': m_deb1_1se, 'asym_se1': asym_se1_1se, 'sigma_hat1': sigma_hat1_1se, 
+                                          'm_deb2': m_deb2_1se, 'asym_se2': asym_se2_1se, 'sigma_hat2': sigma_hat2_1se})
+                deb_prog_1se.to_csv('./Results/DebiasProg_Cirsym_cov_homoerr_d'+str(d)+'_n'+str(n)+\
+                                    '_x'+str(i)+'_beta'+str(k)+'_prop_'+str(non_met)+'_'+str(rule)+'.csv', 
+                                    index=False)
+                    
 
 ## Debiased Lasso (Javanmard and Montarani, 2014)
 for i in range(5):
@@ -155,17 +202,25 @@ for i in range(5):
                           +str(k)+'_laperr.csv', index=False)
 
 ## Debiased Lasso (van de geer et al., 2014)
-
-# lproj_res1 = pd.DataFrame()
-# B = 1000
-# for b in range(1, B+1):
-#     try:
-#         lproj = pd.read_csv('./lproj_res/lproj_res_d'+str(d)+'_n'+str(n)+'_'+str(b)+'_x1.csv')
-#         lproj_res1 = pd.concat([lproj_res1, lproj])
-#     except:
-#         print(b)
-#         continue
-# lproj_res1.to_csv('lproj_res_d'+str(d)+'_n'+str(n)+'_x1.csv', index=False)
+for i in range(5):
+    for k in range(3):
+        lproj_res1 = pd.DataFrame()
+        B = 1000
+        cnt = 0
+        for b in range(1, B+1):
+            try:
+                lproj = pd.read_csv('./lproj_res/lproj_cirsym_d'+str(d)+'_n'+str(n)+'_'+str(b)+'_x'\
+                                +str(i)+'_beta'+str(k)+'.csv')
+                lproj_res1 = pd.concat([lproj_res1, lproj])
+            except:
+                print('DL-Van')
+                print(b)
+                continue
+            cnt += 1
+            if cnt > 100:
+                break
+        lproj_res1.to_csv('./Results/lproj_cirsym_d'+str(d)+'_n'+str(n)+'_x'+str(i)+'_beta'\
+                          +str(k)+'.csv', index=False)
 
 
 
@@ -247,3 +302,16 @@ for i in range(5):
             refit_res0 = pd.concat([refit_res0, refit0])
         refit_res0.to_csv('./Results/refit_Cirsym_d'+str(d)+'_n'+str(n)+'_x'+str(i)+'_beta'+str(k)+'_laperr.csv', 
                           index=False)
+
+
+## Lasso Pilot Estimates
+for i in range(5):
+    for k in range(3):
+        lasso_pilot_res = pd.DataFrame()
+        B = 1000
+        for b in range(1, B+1):
+            lasso_pilot = pd.read_csv('./pilot_res/lasso_pilot_Cirsym_d'+str(d)+'_n'+str(n)+'_'+str(b)+'_x'+str(i)+'_beta'+str(k)+'.csv')
+            lasso_pilot_res = pd.concat([lasso_pilot_res, lasso_pilot])
+        lasso_pilot_res.to_csv('./Results/lasso_pilot_Cirsym_d'+str(d)+'_n'+str(n)+'_x'+str(i)+'_beta'+str(k)+'.csv', 
+                          index=False)
+        
