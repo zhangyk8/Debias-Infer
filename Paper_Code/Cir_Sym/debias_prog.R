b23467d0e5228f257b1ad45bc76b961c8f52305f
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2023-09-08 15:21:42 -0700

    Add files via upload

--- Paper_Code/Cir_Sym/debias_prog.R
+++ Paper_Code/Cir_Sym/debias_prog.R
@@ -20,18 +20,23 @@ DebiasProg = function(X, x, Pi, gamma_n = 0.1) {
     res = psolve(debias_prog)
   }, error = function(e) {
     # res = psolve(debias_prog, solver = "MOSEK", max_iters = 30000)
-    FALSE
+    return(matrix(NA, nrow = n, ncol = 1))
   })
   
-  if(res$value == Inf) {
-    print("The primal debiasing program is infeasible!")
-    return(matrix(NA, nrow = n, ncol = 1))
-  } else if (sum(res[[1]] == "solver_error") > 0){
-    return(matrix(NA, nrow = n, ncol = 1))
-  }
-  else {
-    return(res$getValue(w))
-  }
+  tryCatch({
+    if(res$value == Inf) {
+      print("The primal debiasing program is infeasible!")
+      return(matrix(NA, nrow = n, ncol = 1))
+    } else if (sum(res[[1]] == "solver_error") > 0){
+      return(matrix(NA, nrow = n, ncol = 1))
+    }
+    else {
+      return(res$getValue(w))
+    }
+  }, error = function(e){
+      print("The 'CVXR' fails to solve this program!")
+      return(matrix(NA, nrow = n, ncol = 1))
+  })
 }
 
 
@@ -146,5 +151,70 @@ DualCD = function(X, x, Pi = NULL, gamma_n = 0.05, ll_init = NULL, eps = 1e-9,
   }
   
   return(ll_new)
+}
+
+
+DebiasProgCV = function(X, x, prop_score, gamma_lst = NULL, cv_fold = 5, cv_rule = '1se') {
+  require(caret)
+  if (is.null(gamma_lst)) {
+    gamma_lst = seq(0.001, max(abs(x)), length.out = 41)
+  }
+  
+  kf = createFolds(1:n, cv_fold, list = FALSE, returnTrain = TRUE)
+  dual_loss = matrix(0, nrow = cv_fold, ncol = length(gamma_lst))
+  f_ind = 1
+  
+  for (fold in 1:cv_fold) {
+    train_ind <- (kf != fold)
+    test_ind <- (kf == fold)
+    X_train <- X[train_ind, ]
+    X_test <- X[test_ind, ]
+    prop_score_train <- prop_score[train_ind]
+    prop_score_test <- prop_score[test_ind]
+    
+    for (j in 1:length(gamma_lst)) {
+      w_train = DebiasProg(X = X_train, x = x, Pi = diag(prop_score_train), gamma_n = gamma_lst[j])
+      
+      if (any(is.na(w_train))) {
+        cat(paste("The primal debiasing program for this fold of the data is not feasible when gamma/n=", round(gamma_lst[j], 4), "!\n"))
+        dual_loss[f_ind, j] = NA
+      } else {
+        ll_train = DualCD(X = X_train, x = x, Pi = diag(prop_score_train), gamma_n = gamma_lst[j], ll_init = NULL, eps = 1e-8, max_iter = 5000)
+        
+        if (sum(abs(w_train + drop(X_train %*% ll_train) / (2 * sqrt(dim(X_train)[1]))) > 1e-3) > 0) {
+          cat(paste("The strong duality between primal and dual programs does not satisfy when gamma/n=", round(gamma_lst[j], 4), "!\n"))
+          dual_loss[f_ind, j] = NA
+        } else {
+          dual_loss[f_ind, j] = DualObj(X_test, x = x, Pi = diag(prop_score_test), ll_cur = ll_train, gamma_n = gamma_lst[j])
+        }
+      }
+    }
+    
+    f_ind = f_ind + 1
+  }
+  
+  mean_dual_loss = apply(dual_loss, 2, mean, na.rm = FALSE)
+  std_dual_loss = apply(dual_loss, 2, function(x) sd(x, na.rm = FALSE)) / sqrt(cv_fold)
+  
+  if (cv_rule == 'mincv') {
+    gamma_n_opt = gamma_lst[which.min(mean_dual_loss)]
+  }
+  if (cv_rule == '1se') {
+    One_SE = (mean_dual_loss > min(mean_dual_loss, na.rm = TRUE) + std_dual_loss[which.min(mean_dual_loss)]) &
+      (gamma_lst < gamma_lst[which.min(mean_dual_loss)])
+    if (sum(One_SE, na.rm = TRUE) == 0) {
+      One_SE = rep(TRUE, length(gamma_lst))
+    }
+    gamma_lst = gamma_lst[One_SE]
+    gamma_n_opt = gamma_lst[which.min(mean_dual_loss[One_SE])]
+  }
+  if (cv_rule == 'minfeas') {
+    gamma_n_opt = min(gamma_lst[!is.na(mean_dual_loss)])
+  }
+  
+  w_obs = DebiasProg(X = X, x = x, Pi = diag(prop_score), gamma_n = gamma_n_opt)
+  ll_obs = DualCD(X = X, x = x, Pi = diag(prop_score), gamma_n = gamma_n_opt, ll_init = NULL, eps = 1e-9)
+  
+  return(list(w_obs = w_obs, ll_obs = ll_obs, gamma_n_opt = gamma_n_opt))
 }
 
